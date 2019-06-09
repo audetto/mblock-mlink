2247b9dd372bd47345ff296f879e9aba514a49fa
Andrea Odetti <mariofutire@gmail.com>
2019-06-09 15:46:48 +0100

    Initial import from
    https://dl.makeblock.com/mblock5/linux/mLink-1.2.0-1.el7.x86_64.rpm
    
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

---
+++ constant.js
@@ -0,0 +1,1 @@
+"use strict";var ROUTER={CONNECT_TYPE:{SERIALPORT:"serialport",WLAN_SCAN:"wlanScan"}};module.exports={ROUTER:ROUTER};
---
+++ platforms/wlanScan/env.js
@@ -0,0 +1,1 @@
+"use strict";var util=require("util");module.exports=util._extend(process.env,{LANG:"en",LC_ALL:"en",LC_MESSAGES:"en"});
